dbbe64e86d4fba58eb6c7d9e55c00985ba5b3a2f
Stephen Kang <kang.stephen94@gmail.com>
2018-07-25 14:31:57 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require(`express`)
+const fetch = require(`node-fetch`)
+const app = express()
+const PORT = process.env.PORT || 8000
+
+const getPrice = function (data) {
+  return data.quotes.USD.price
+}
+
+const getVolume= function (data) {
+  return data.quotes.USD.volume_24h
+}
+
+app.listen(PORT, () => {
+  console.log(__dirname)
+  console.log(`listening on ${PORT}`)
+})
+
+app.get(`/`, (req, res) => {
+  fetch(`https://api.coinmarketcap.com/v2/ticker/?start=101&limit=10&sort=volume_24h&structure=array`)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(json => {
+      console.log(`Top 10 CryptoCurrencies with largest 24h volume (coinmarketcap)`)
+      json.data.forEach(token => {
+        console.log(`${token.name}: $${getPrice(token)}`)
+      })
+    })
+})
---
+++ public/javascript/index.js
@@ -0,0 +1,7 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+document.addEventListener('DOMContentLoaded', () => {
+  const main = document.getElementById('main')
+  ReactDOM.render(<hello />, main)
+})
